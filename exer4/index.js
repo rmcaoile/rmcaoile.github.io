3d3be3c2a97fba8d71e8d667db2e2d1e77c5a958
rmcaoile <rmcaoile@up.edu.ph>
2024-03-08 12:42:21 +0800

    chore: created exer4 index.js and test.js

--- exer4/index.js
+++ exer4/index.js
@@ -1,13 +1,98 @@
-// const math = require('./math.js');
-// the import can be named anything
+import { v4 as uuidv4 } from 'uuid';
+import validator from 'validator';
+import { appendFileSync } from 'node:fs';
 
-import math from './math.js'
-import { snakeCase } from 'snake-case';
+const generateUniqueID = (fname, lname) => {
+    var fnameInitial = fname[0].toLowerCase();
+    var lowercaseLname = lname.toLowerCase();
+    var fullUuid = uuidv4();
+    var idNum = "";
 
-console.log(math.add(5, 2)); // 7
-console.log(math.mul(5, 2)); // 10
+    for (let i=0; i<8; i++){
+        idNum += fullUuid[i];
+    }
 
-console.log(snakeCase('thisIsInCamelCase'));
+    return(fnameInitial + lowercaseLname + idNum); 
+  }
 
-console.log(math.PI * (5 ** 2)); 
-console.log(math.obj.one + math.obj.two); // 2
+const addAccount = (array) => {
+    if (array.length == 4) {
+        console.log("All fields present");
+        
+        if (array[0] != "" && array[1] != "" && array[2] != "" ) {
+            console.log("Non-empty strings"); 
+            
+            if (validator.isEmail(array[2])) {
+                console.log("Valid email format");
+
+                if (array[3] > 17) {
+                    console.log("Age is at least 18");
+
+                    try {
+                        appendFileSync('users.txt', array[0]+", "+ array[1]+", "+array[2]+", "+array[3]+", "+generateUniqueID('Alan', 'Turing') +"\n");
+                        console.log('APPENDING SUCCESS!\n');
+
+                      } catch (err) {
+                        console.log('APPENDING FAILED!\n');                      
+                      }
+                }
+                else {
+                    console.log("Age is below 18!");
+                    console.log('APPENDING FAILED!\n'); 
+                }
+            }
+            else {
+                console.log("Invalid email format!"); 
+                console.log('APPENDING FAILED!\n'); 
+            }
+        }
+        else {
+            console.log("Empty strings!");   
+            console.log('APPENDING FAILED!\n'); 
+        }            
+    }
+    else {
+        console.log("Some fields missing!");
+        console.log('APPENDING FAILED!\n'); 
+    }
+    
+
+}
+
+export default {generateUniqueID, addAccount}
+
+
+
+
+
+// const sampleArray = ["Allan", "Turing", "aturing@w3c.com", 58];
+
+
+// addAccount(sampleArray);
+
+// generateUniqueID('Alan', 'Turing');
+// console.log(addAccount(sampleArray))
+
+
+
+
+
+
+
+
+
+
+
+// // const math = require('./math.js');
+// // the import can be named anything
+
+// import math from './math.js'
+// import { snakeCase } from 'snake-case';
+
+// console.log(math.add(5, 2)); // 7
+// console.log(math.mul(5, 2)); // 10
+
+// console.log(snakeCase('thisIsInCamelCase'));
+
+// console.log(math.PI * (5 ** 2)); 
+// console.log(math.obj.one + math.obj.two); // 2
---
+++ exer4/test.js
@@ -0,0 +1,22 @@
+import accountGenerator from './index.js'
+
+
+const sampleArray1 = ["Allan", "Turing", "aturing@w3c.com", 60];
+const sampleArray2 = ["Tim", "tim@w3c.com", 25];
+const sampleArray3 = ["", "Nelson", "ted.n@w3c.com", 43];
+const sampleArray4 = ["Tim", "Berners-Lee", "timw3c.com", 25];
+const sampleArray5 = ["Ted", "Nelson", "ted.n@w3c.com", 17];
+
+
+const sampleArray6 = ["Tim", "Berners-Lee", "tim@w3c.com", 25];
+const sampleArray7 = ["Ted", "Nelson", "ted.n@w3c.com", 43];
+
+
+accountGenerator.addAccount(sampleArray1);
+accountGenerator.addAccount(sampleArray2);
+accountGenerator.addAccount(sampleArray3);
+accountGenerator.addAccount(sampleArray4);
+accountGenerator.addAccount(sampleArray5);
+accountGenerator.addAccount(sampleArray6);
+accountGenerator.addAccount(sampleArray7);
+
