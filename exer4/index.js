31b00103656ae408e24a3f4491670d8361b9aa31
rmcaoile <rmcaoile@up.edu.ph>
2024-03-08 12:57:37 +0800

    docs: added documentation to index.js and test.js

--- exer4/index.js
+++ exer4/index.js
@@ -1,98 +1,73 @@
+// Importing necessary packages
 import { v4 as uuidv4 } from 'uuid';
 import validator from 'validator';
 import { appendFileSync } from 'node:fs';
 
+// Function that generates UniqueID
 const generateUniqueID = (fname, lname) => {
-    var fnameInitial = fname[0].toLowerCase();
-    var lowercaseLname = lname.toLowerCase();
-    var fullUuid = uuidv4();
-    var idNum = "";
+    var fnameInitial = fname[0].toLowerCase();      // Getting the first character of fname then converting it to lowercase
+    var lowercaseLname = lname.toLowerCase();       // Converting lname to lowercase
+    var fullUuid = uuidv4();                        // Generating original uuid
+    var idNum = "";                                 // Variable to store 8 character uuid
 
+    // Loop to get 8 character uuid
     for (let i=0; i<8; i++){
         idNum += fullUuid[i];
     }
 
+    // Returning the generated unique id
     return(fnameInitial + lowercaseLname + idNum); 
   }
 
+// Function that add account to user.txt
 const addAccount = (array) => {
-    if (array.length == 4) {
+    if (array.length == 4) {        // Check if all fields are present
         console.log("All fields present");
         
-        if (array[0] != "" && array[1] != "" && array[2] != "" ) {
+        if (array[0] != "" && array[1] != "" && array[2] != "" ) {      // Check if the first name, last name, and email are non-empty strings
             console.log("Non-empty strings"); 
             
-            if (validator.isEmail(array[2])) {
+            if (validator.isEmail(array[2])) {      // Check if the email is in a valid format 
                 console.log("Valid email format");
 
                 if (array[3] > 17) {
-                    console.log("Age is at least 18");
+                    console.log("Age is at least 18");      // Check if age is at least 18
 
                     try {
-                        appendFileSync('users.txt', array[0]+", "+ array[1]+", "+array[2]+", "+array[3]+", "+generateUniqueID('Alan', 'Turing') +"\n");
+                        appendFileSync('users.txt', array[0]+", "+ array[1]+", "+array[2]+", "+array[3]+", "+generateUniqueID(array[0], array[1]) +"\n");     // Appending the data to users.txt
                         console.log('APPENDING SUCCESS!\n');
 
-                      } catch (err) {
+                      } catch (err) {       // Error catcher
                         console.log('APPENDING FAILED!\n');                      
                       }
                 }
                 else {
-                    console.log("Age is below 18!");
+                    // Prompt if Age is below 18
+                    console.log("Age is below 18!");        
                     console.log('APPENDING FAILED!\n'); 
                 }
             }
             else {
+                // Prompt if Invalid email format
                 console.log("Invalid email format!"); 
                 console.log('APPENDING FAILED!\n'); 
             }
         }
         else {
-            console.log("Empty strings!");   
+            // Prompt if theres an Empty strings
+            console.log("Empty strings exist!");   
             console.log('APPENDING FAILED!\n'); 
         }            
     }
     else {
-        console.log("Some fields missing!");
+        // Prompt if Some fields are missing
+        console.log("Some fields are missing!");
         console.log('APPENDING FAILED!\n'); 
     }
     
 
 }
 
+// Export the functions generateUniqueID and addAccount
 export default {generateUniqueID, addAccount}
 
-
-
-
-
-// const sampleArray = ["Allan", "Turing", "aturing@w3c.com", 58];
-
-
-// addAccount(sampleArray);
-
-// generateUniqueID('Alan', 'Turing');
-// console.log(addAccount(sampleArray))
-
-
-
-
-
-
-
-
-
-
-
-// // const math = require('./math.js');
-// // the import can be named anything
-
-// import math from './math.js'
-// import { snakeCase } from 'snake-case';
-
-// console.log(math.add(5, 2)); // 7
-// console.log(math.mul(5, 2)); // 10
-
-// console.log(snakeCase('thisIsInCamelCase'));
-
-// console.log(math.PI * (5 ** 2)); 
-// console.log(math.obj.one + math.obj.two); // 2
--- exer4/test.js
+++ exer4/test.js
@@ -1,17 +1,20 @@
+// Importing functions from index.js for testing
 import accountGenerator from './index.js'
 
+// Successful appending
+const sampleArray1 = ["Allan", "Turing", "aturing@w3c.com", 60];    
 
-const sampleArray1 = ["Allan", "Turing", "aturing@w3c.com", 60];
+// Failed appending
 const sampleArray2 = ["Tim", "tim@w3c.com", 25];
 const sampleArray3 = ["", "Nelson", "ted.n@w3c.com", 43];
 const sampleArray4 = ["Tim", "Berners-Lee", "timw3c.com", 25];
 const sampleArray5 = ["Ted", "Nelson", "ted.n@w3c.com", 17];
 
-
+// Successful appending
 const sampleArray6 = ["Tim", "Berners-Lee", "tim@w3c.com", 25];
 const sampleArray7 = ["Ted", "Nelson", "ted.n@w3c.com", 43];
 
-
+// Calling of addAccount funtion to test the program
 accountGenerator.addAccount(sampleArray1);
 accountGenerator.addAccount(sampleArray2);
 accountGenerator.addAccount(sampleArray3);
