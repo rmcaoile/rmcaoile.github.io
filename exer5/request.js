59b433ef9ed3e859f97ab02cfdca973c3511663a
rmcaoile <rmcaoile@up.edu.ph>
2024-03-15 23:08:23 +0800

    chore: finished exer5 with documentation

--- exer5/request.js
+++ exer5/request.js
@@ -1,39 +1,53 @@
 import needle from 'needle';
 
-needle.post(    
-    'http://localhost:3000/add-books',
-    { book_name: "Harry Potter and the Philosopher’s Stone",
-      isbn: "978-0-7475-3269-9",
-      author: "J.K Rowling",
-      year_published: "",
-    },
-    (err, res) => {
-      console.log(res.body)   // prints the server’s response “Received a POST request.”
-    }
+// Sample 
+needle.post(
+  'http://localhost:3000/add-books',
+  {
+    book_name: "Harry Potter and the Philosopher’s Stone",
+    isbn: "978-0-7475-3269-9",
+    author: "J.K Rowling",
+    year_published: "1997",
+  },
+  (err, res) => {
+    console.log(res.body)   // prints the server’s response “{ success: true } or { success: false }”
+  }
 );
 
-// needle.get('http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9', (err, res) => {
-//     // console.log(res.body);   // prints the body of the response message. In this case, “Hello”
-//     console.log(res.body);
-// });
+needle.post(
+  'http://localhost:3000/add-books',
+  {
+    book_name: "Harry Potter and the Chamber of Secrets",
+    isbn: "0-7475-3849-2",
+    author: "J.K Rowling",
+    year_published: "1998",
+  },
+  (err, res) => {
+    console.log(res.body)   // prints the server’s response “{ success: true } or { success: false }”   
+  }
+);
+
+needle.post(
+  'http://localhost:3000/add-books',
+  {
+    book_name: "The Little Prince",
+    isbn: "978-0156012195",
+    author: "Antoine Saint-Exupery",
+    year_published: "1943",
+  },
+  (err, res) => {
+    console.log(res.body)   // prints the server’s response “{ success: true } or { success: false }”   
+  }
+);
+
+// Request GET method for retrieving the value of ISBN and Author
+needle.get('http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9&author=J.K+Rowling', (err, res) => {
+  console.log(res.body);   // prints the server’s response which is value of ISBN and Author”
+});
+
+// Same request GET method as #2 but with Author as the only parameter.
+needle.get('http://localhost:3000/find-by-author?author=J.K+Rowling', (err, res) => {
+  console.log(res.body);   // prints the server’s response which is value of Author”
+});
 
 
-// needle.get('http://localhost:3000/', (err, res) => {
-//     console.log(res.body);   // prints the body of the response message. In this case, “Hello”
-//     console.log(res.statusCode);
-// });
-
-// needle.get('http://localhost:3000/greeting?name=Ralph', (err, res) => {
-//     // console.log(res.body);   // prints the body of the response message. In this case, “Hello”
-//     console.log(res.body);
-// });
-
-// needle.post(    
-//     'http://localhost:3000/submit-data',
-//     {name: "Ralph",
-//      age: "21"},
-//     (err, res) => {
-//       console.log(res.body)   // prints the server’s response “Received a POST request.”
-//     }
-// );
-  
--- exer5/server.js
+++ exer5/server.js
@@ -6,40 +6,44 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post('/add-books', (req, res) => {  
-    // res.json({success: true})  
-    // addBook(req, res);     // call saveBook function
+app.post('/add-books', (req, res) => {
+    const book_data = [];     // Initializing array to insert book data
 
-    const book_detail = [];
-
-    book_detail.push(req.body.book_name);
-    book_detail.push(req.body.isbn);
-    book_detail.push(req.body.author);
-    book_detail.push(req.body.year_published);
+    // Appending values from request to the array
+    book_data.push(req.body.book_name);
+    book_data.push(req.body.isbn);
+    book_data.push(req.body.author);
+    book_data.push(req.body.year_published);
 
 
-    if (book_detail.length == 4) {        // Check if all fields are present
-        console.log("All fields present");        
-        for (let i=0; i<4; i++){
-            if (book_detail[i] != "") {
-                // console.log("All field are not empty strings");                 
-                appendFileSync('books.txt', book_detail[0]+", "+ book_detail[1]+", "+book_detail[2]+", "+book_detail[3]+"\n");     // Appending the data to books.txt
-                res.json({success: true})                
-            }
-            else {
-                res.json({success: false})     
-            }
+    if (book_data[0] != undefined && book_data[1] != undefined && book_data[2] != undefined && book_data[3] != undefined) {        // Check if all fields are present
+        // console.log("All fields present");
+        if (book_data[0] != "" && book_data[1] != "" && book_data[2] != "" && book_data[3] != "") {        // Check if no entries are empty strings
+            // console.log("All field are not empty strings");                 
+            appendFileSync('books.txt', book_data[0] + ", " + book_data[1] + ", " + book_data[2] + ", " + book_data[3] + "\n");     // Appending the data to books.txt
+            res.json({ success: true })         // Response to the successful request
+        }
+        else {
+            res.json({ success: false })        // Response to the unsuccessful request due to empty string input
         }
     }
     else {
-        res.json({success: false})     
+        res.json({ success: false })            // Response to the unsuccessful request due to missing input
     }
+}
 
+);
+
+// Response GET method for retrieving the value of ISBN and Author
+app.get('/find-by-isbn-author', (req, res) => {
+    res.send('\nISBN: ' + req.query.isbn + "\nAuthor: " + req.query.author);
+    // console.log(req);
 });
 
-app.get('/find-by-isbn-author', (req, res) => {
-    res.send('Hello ' + req.query.name);
-    console.log(req);
+// Same response GET method as #2 but with Author as the only parameter.
+app.get('/find-by-author', (req, res) => {
+    res.send("\nAuthor: " + req.query.author);
+    // console.log(req);
 });
 
 
@@ -47,57 +51,12 @@ app.get('/find-by-isbn-author', (req, res) => {
 // this tells our app to listen for GET messages on the '/' path
 // the callback function specifies what the server will do when a message is received
 app.get('/', (req, res) => {
-    res.send('Hello! this is Ralph\'s server');
-//   console.log(res);
-//   console.log(req);
-});
-
-app.post('/submit-data', (req, res) => {
-    res.send('Received a POST request from ' + req.body.name + '\nTheir age is ' + req.body.age);    
-    // res.send('Their age is ' + req.body.age);
-    res.json({success: true})
-    // ('Received a POST request from ' + req.body.name + '\nTheir age is ' + req.body.age);    
-    
-});
-
-app.get('/greeting', (req, res) => {
-    res.send('Hello ' + req.query.name);
-    console.log(req);
+    res.send('Hello! This is Ralph Philip\'s server');
+    //   console.log(res);
+    //   console.log(req);
 });
 
 // this tells our server to listen to the port 3000
 // we can also pass an optional callback function to execute after the server starts
-app.listen(3000, () => { console.log('Server started at port 3000')} );
+app.listen(3000, () => { console.log('Server started at port 3000') });
 
-
-
-
-
-// Function that add account to user.txt
-const addBook = (res, req) => {
-    res.json({success: true});
-    const book_detail = [];
-
-    book_detail.push(req.body.book_name);
-    book_detail.push(req.body.isbn);
-    book_detail.push(req.body.author);
-    book_detail.push(req.body.year_published);
-
-
-    if (book_detail.length == 4) {        // Check if all fields are present
-        console.log("All fields present");        
-        for (let i=0; i<4; i++){
-            if (book_detail[i] != "") {
-                console.log("All field are not empty strings");                 
-                appendFileSync('books.txt', book_detail[0]+", "+ book_detail[1]+", "+book_detail[2]+", "+book_detail[3]+"\n");     // Appending the data to books.txt
-                res.json({success: true})                
-            }
-            else {
-                res.json({success: false})     
-            }
-        }
-    }
-    else {
-        res.json({success: false})     
-    } 
-}
