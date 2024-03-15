40c6c2646bc5e805d385c3acf4cf5f0f77227abb
rmcaoile <rmcaoile@up.edu.ph>
2024-03-15 12:57:00 +0800

    chore: partial progress in exer5

--- exer5/request.js
+++ exer5/request.js
@@ -1,21 +1,39 @@
 import needle from 'needle';
 
-needle.get('http://localhost:3000/', (err, res) => {
-    console.log(res.body);   // prints the body of the response message. In this case, “Hello”
-    console.log(res.statusCode);
-});
-
-needle.get('http://localhost:3000/greeting?name=Ralph', (err, res) => {
-    // console.log(res.body);   // prints the body of the response message. In this case, “Hello”
-    console.log(res.body);
-});
-
 needle.post(    
-    'http://localhost:3000/submit-data',
-    {name: "Ralph",
-     age: "21"},
+    'http://localhost:3000/add-books',
+    { book_name: "Harry Potter and the Philosopher’s Stone",
+      isbn: "978-0-7475-3269-9",
+      author: "J.K Rowling",
+      year_published: "",
+    },
     (err, res) => {
       console.log(res.body)   // prints the server’s response “Received a POST request.”
     }
-  );
+);
+
+// needle.get('http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9', (err, res) => {
+//     // console.log(res.body);   // prints the body of the response message. In this case, “Hello”
+//     console.log(res.body);
+// });
+
+
+// needle.get('http://localhost:3000/', (err, res) => {
+//     console.log(res.body);   // prints the body of the response message. In this case, “Hello”
+//     console.log(res.statusCode);
+// });
+
+// needle.get('http://localhost:3000/greeting?name=Ralph', (err, res) => {
+//     // console.log(res.body);   // prints the body of the response message. In this case, “Hello”
+//     console.log(res.body);
+// });
+
+// needle.post(    
+//     'http://localhost:3000/submit-data',
+//     {name: "Ralph",
+//      age: "21"},
+//     (err, res) => {
+//       console.log(res.body)   // prints the server’s response “Received a POST request.”
+//     }
+// );
   
--- exer5/server.js
+++ exer5/server.js
@@ -1,9 +1,48 @@
 import express from 'express';
+import { appendFileSync } from 'node:fs';
 
 // instantiate the server
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.post('/add-books', (req, res) => {  
+    // res.json({success: true})  
+    // addBook(req, res);     // call saveBook function
+
+    const book_detail = [];
+
+    book_detail.push(req.body.book_name);
+    book_detail.push(req.body.isbn);
+    book_detail.push(req.body.author);
+    book_detail.push(req.body.year_published);
+
+
+    if (book_detail.length == 4) {        // Check if all fields are present
+        console.log("All fields present");        
+        for (let i=0; i<4; i++){
+            if (book_detail[i] != "") {
+                // console.log("All field are not empty strings");                 
+                appendFileSync('books.txt', book_detail[0]+", "+ book_detail[1]+", "+book_detail[2]+", "+book_detail[3]+"\n");     // Appending the data to books.txt
+                res.json({success: true})                
+            }
+            else {
+                res.json({success: false})     
+            }
+        }
+    }
+    else {
+        res.json({success: false})     
+    }
+
+});
+
+app.get('/find-by-isbn-author', (req, res) => {
+    res.send('Hello ' + req.query.name);
+    console.log(req);
+});
+
+
 
 // this tells our app to listen for GET messages on the '/' path
 // the callback function specifies what the server will do when a message is received
@@ -29,3 +68,36 @@ app.get('/greeting', (req, res) => {
 // this tells our server to listen to the port 3000
 // we can also pass an optional callback function to execute after the server starts
 app.listen(3000, () => { console.log('Server started at port 3000')} );
+
+
+
+
+
+// Function that add account to user.txt
+const addBook = (res, req) => {
+    res.json({success: true});
+    const book_detail = [];
+
+    book_detail.push(req.body.book_name);
+    book_detail.push(req.body.isbn);
+    book_detail.push(req.body.author);
+    book_detail.push(req.body.year_published);
+
+
+    if (book_detail.length == 4) {        // Check if all fields are present
+        console.log("All fields present");        
+        for (let i=0; i<4; i++){
+            if (book_detail[i] != "") {
+                console.log("All field are not empty strings");                 
+                appendFileSync('books.txt', book_detail[0]+", "+ book_detail[1]+", "+book_detail[2]+", "+book_detail[3]+"\n");     // Appending the data to books.txt
+                res.json({success: true})                
+            }
+            else {
+                res.json({success: false})     
+            }
+        }
+    }
+    else {
+        res.json({success: false})     
+    } 
+}
